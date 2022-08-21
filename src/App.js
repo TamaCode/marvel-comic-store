3557d3aa2a7ecbab46dcb98e6ea902b753dc5c17
Martin Tamareu <mg.tamareu@gmail.com>
2022-08-20 22:31:06 -0300

    Se crea componente ItemListContainer con sus estilos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar.js';
 import Title from './components/Title/Title.js';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer.js';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Navbar />
       </header>
       <body>
+        <ItemListContainer greeting='Bienvenid@ a la Tienda Online de Comics de Marvel!' />
         <Title title='Explore Our Comics Store !' />
       </body>
     </div>
