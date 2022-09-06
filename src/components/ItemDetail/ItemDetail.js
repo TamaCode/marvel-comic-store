ea2ffc0dc871a3814e4a9cb1c8f1598532d3c8c2
Martin Tamareu <mg.tamareu@gmail.com>
2022-09-05 21:30:31 -0300

    Corrijo nombre de clases del componente ItemDetail

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -4,12 +4,12 @@ import Button from 'react-bootstrap/Button';
 const ItemDetail = ({ comic }) => {
   return (
     <div className="item-detail-box">
-      <div className="comic-img-box">
-        <img className='comic-img' src={comic.img_url} alt="" />
+      <div className="item-img-box">
+        <img className='item-img' src={comic.img_url} alt="" />
       </div>
       
       <div className='item-data-box'>
-        <h3 className='comic-title'>{comic.title}</h3>
+        <h3 className='item-title'>{comic.title}</h3>
         <p><b>{comic.resumed_description}</b></p>
         <h5 className='price-text'><b><i>US$ {comic.price}</i></b></h5>
         <a href=""><Button variant='danger' className="details-botton" >See Item Details</Button></a>
