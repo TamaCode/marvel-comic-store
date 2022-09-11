0878b28e254aa3e7b158870d51589a9f3bafc601
Martin Tamareu <mg.tamareu@gmail.com>
2022-09-10 22:05:37 -0300

    Construyo modal de CART

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,14 +1,37 @@
 import { useState, useEffect } from "react";
+import Button from 'react-bootstrap/Button';
+import marvel_logo from '../Title/marvel_logo.svg';
 import './Cart.css';
 
-const Cart = (/*{show}*/) => {
+const Cart = ({show}) => {
   return (
-          // <div className="modal-box">
-            <div className="modal-window">
-              <p>Mi Carrito</p>
-              {/* <button onClick={show}>Cerrar</button> */}
-            </div>
-          // </div>
+    <div className="modal-box">
+      <div className="modal-window">
+        <div className="cart-title-box">
+          <img src={marvel_logo} alt='Marvel Comics Logo' />
+          <h3 className="cart-title">YOUR CART</h3>
+          <img src={marvel_logo} alt='Marvel Comics Logo' />
+        </div>
+        <div><p>Mi Carrito1</p></div>
+        <div><p>Mi Carrito2</p></div>
+        <div><p>Mi Carrito3</p></div>
+        <div><p>Mi Carrito4</p></div>
+        <div><p>Mi Carrito5</p></div>
+        <div><p>Mi Carrito6</p></div>
+        <div><p>Mi Carrito7</p></div>
+        <div><p>Mi Carrito8</p></div>
+        <div><p>Mi Carrito9</p></div>
+        <div><p>Mi Carrito10</p></div>
+        <div><p>Mi Carrito11</p></div>
+        <div><p>Mi Carrito12</p></div>
+        <div><p>Mi Carrito13</p></div>
+        <div><p>Mi Carrito14</p></div>
+        <div><p>Mi Carrito15</p></div>
+        <div className="close-button-box">
+          <Button className="close-button" variant='danger' onClick={show}>Cerrar</Button>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,13 +1,28 @@
 import Button from 'react-bootstrap/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
+import Cart from '../Cart/Cart.js';
 import  { Link } from 'react-router-dom';
 
 const CartWidget = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const viewModal = () => {
+    console.log('open modal')
+    setShowModal(!showModal);
+  };
   return (
+    <>
     <li className="cart-box">
-      <Link to="/cart"><Button variant='danger'><span className='menu-icon'><FontAwesomeIcon icon={faCartShopping} /></span>CART</Button></Link>
+      <a /*to="/cart"*/><Button onClick={viewModal} variant='danger'><span className='menu-icon'><FontAwesomeIcon icon={faCartShopping} /></span>CART</Button></a>
     </li>
+    {
+      showModal ? 
+      <Cart show={viewModal}></Cart> :
+      null
+    }
+    </>
   );
 };
 
